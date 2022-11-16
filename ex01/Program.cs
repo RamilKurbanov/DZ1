70c6094631d1dd343ffcc82e74824368ecf96e29
Ramil <ramilkurbanov666@gmail.com>
2022-11-16 19:43:16 +0300

    задание по 4 семинару

---
+++ ex01/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] ThreeDigitNumbers(int size)
+{
+    int [] threee_digit = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        threee_digit[i] = new Random().Next(100,1000);
+    }
+    return threee_digit;
+}
+
+int Quantity_of_EvenNumbers(int[] array)
+{
+    int count = 0;
+    foreach(int element in array)
+    {
+        if(element % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write("Введите размер массива: ");
+int number = int.Parse(Console.ReadLine()!);
+int[] WorkingArray = ThreeDigitNumbers(number);
+Console.WriteLine($"Полученный массив: [{String.Join(", ", WorkingArray)}]");
+Console.Write($"Количесво четных чисел в массиве: {Quantity_of_EvenNumbers(WorkingArray)}]");
---
+++ ex03/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] Return_Array(int size, int left, int right)
+{
+    double[] vector = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        vector[i] = Math.Round(new Random().NextDouble() * ( right - left) + left, 5);
+    }
+    return vector;
+}
+
+double MaxElement(double[] array)
+{
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+double MinElement (double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+
+Console.Write("Введит размер массива: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значения левого конца диапазона: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значения правого конца диапазона: ");
+int b = int.Parse(Console.ReadLine()!);
+double[] WorkingArray = Return_Array(number, a, b);
+Console.WriteLine($"Полученный массив: [{String.Join(", ", WorkingArray)}]");
+
+Console.WriteLine($"Наименьший элмент массива: {MinElement(WorkingArray): f5}");
+Console.WriteLine($"Наименьший элмент массива: {MaxElement(WorkingArray): f5}");
+
+double sum = MaxElement(WorkingArray) + MinElement(WorkingArray);
+
